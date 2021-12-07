6b618d5d03e7d41d57471985e5aab4cac24aa4bc
marcopiraccini <marco.piraccini@gmail.com>
2021-12-07 07:50:48 +0100

    day07, cleaned code

--- src/day07.rs
+++ src/day07.rs
@@ -1,40 +1,34 @@
-fn parse_input(mut input: String) -> Vec<usize> {
+fn parse_input(mut input: String) -> Vec<isize> {
     input.pop(); // removes the trailing \n
     input
         .split(',')
-        .map(|x| x.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>()
+        .map(|x| x.parse::<isize>().unwrap())
+        .collect::<Vec<isize>>()
 }
 
 pub fn part1(input: String) {
     let mut crabs = parse_input(input.clone());
     crabs.sort();
     let median = crabs[crabs.len() / 2];
-
-    let res = crabs
-        .iter()
-        .fold(0, |acc, c| acc + isize::abs(*c as isize - median as isize));
+    let res = crabs.iter().fold(0, |acc, &c| acc + isize::abs(c - median));
     println!("Solution part 1: {:?}", res);
 }
 
-fn cost(n: isize) -> usize {
+fn cost(n: isize) -> isize {
     let mut res = 0;
     for i in 0..isize::abs(n) + 1 {
         res += i;
     }
-    res as usize
+    res as isize
 }
 
 pub fn part2(input: String) {
     let mut crabs = parse_input(input.clone());
     crabs.sort();
     let max = *(crabs.iter().max().unwrap());
-    let mut min = 10000000000;
-    // Yes, this can be optimized, but is not really necessary in this case...
+    let mut min = isize::MAX; // Yes, this can be optimized, but is not really necessary in this case...
     for n in 0..max {
-        let total_cost = crabs
-            .iter()
-            .fold(0, |acc, c| acc + cost(*c as isize - n as isize));
+        let total_cost = crabs.iter().fold(0, |acc, &c| acc + cost(c - n));
         if total_cost < min {
             min = total_cost;
         }
@@ -44,11 +38,8 @@ pub fn part2(input: String) {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn day07_part2() {
-        let mut res = cost(16 - 5);
-        assert_eq!(res, 66);
+        assert_eq!(super::cost(16 - 5), 66);
     }
 }
